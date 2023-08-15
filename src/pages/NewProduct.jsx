d4199227f189bfb4137d99618fa9413979867eb0
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2023-08-14 22:07:18 -0400

    Cloudinary Upload API
    upload images => return URL
    
    https://cloudinary.com/documentation/upload_images#unauthenticated_requests

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from '../components/ui/Button';
+import { uploadImage } from '../api/uploader';
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
@@ -15,6 +16,9 @@ export default function NewProduct() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    uploadImage(file).then((url) => {
+      console.log(url);
+    });
   };
 
   return (
