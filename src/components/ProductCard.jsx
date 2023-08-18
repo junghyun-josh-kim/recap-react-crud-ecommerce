5ddc2bd1f690aad8e663fcc32479ab62b14df690
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2023-08-17 22:06:19 -0400

    Banner: Initial structure
    HTML

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,16 +4,17 @@ export default function ProductCard({
   product: { id, image, title, category, price },
 }) {
   return (
-    <li>
+    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer hover:brightness-110 hover:scale-105 transition ease-in-out'>
       <img
+        className='w-full'
         src={image}
         alt={title}
       />
-      <div>
-        <h3>{title}</h3>
+      <div className='mt-2 px-2 text-lg flex justify-between items-center'>
+        <h3 className='truncate'>{title}</h3>
         <p>{`$${price}`}</p>
       </div>
-      <p>{category}</p>
+      <p className='mb-2 px-2 text-gray-400'>{category}</p>
     </li>
   );
 }
