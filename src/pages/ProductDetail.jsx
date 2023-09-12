9f947bcb81a19d5bc9eb9cc29a5e0d68dc86d7c9
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2023-09-11 21:59:13 -0400

    Product Detail: CSS

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -12,31 +12,46 @@ export default function ProductDetail() {
   const handleSelect = (e) => setSelected(e.target.value);
   const handleClick = (e) => {};
   return (
-    <section>
-      <p>{category}</p>
-      <img
-        src={image}
-        alt={title}
-      />
-      <div>
-        <h2>{title}</h2>
-        <p>${price}</p>
-        <p>{description}</p>
-        <p>Option:</p>
-        <select
-          onChange={handleSelect}
-          value={selected}
-        >
-          {options &&
-            options.map((option, index) => (
-              <option key={index}>{option}</option>
-            ))}
-        </select>
-        <Button
-          text='Add Cart'
-          onClick={handleClick}
+    <>
+      <p className='mx-8 mt-6 text-gray-700'>{category}</p>
+      <section className='flex flex-col md:flex-row p-4'>
+        <img
+          className='w-full m-2 basis-7/12 rounded-xl'
+          src={image}
+          alt={title}
         />
-      </div>
-    </section>
+        <div className='w-full basis-5/12 flex flex-col p-4'>
+          <h2 className='text-3xl font-bold py-2 border-b border-gray-400'>
+            {title}
+          </h2>
+          <p className='text-l font-bold py-2'>${price}</p>
+          <p>{description}</p>
+          <div className='flex items-center'>
+            <label
+              className='text-brand font-bold'
+              htmlFor='select'
+            >
+              Option:
+            </label>
+            <select
+              id='select'
+              className='p-2 my-3 ml-2 flex-1 border-2 border-dashe outline-none capitalize rounded-md'
+              onChange={handleSelect}
+              value={selected}
+            >
+              {options &&
+                options.map((option, index) => (
+                  <option key={index}>{option}</option>
+                ))}
+            </select>
+          </div>
+          <Button
+            text='Add Cart'
+            style='font-bold'
+            onClick={handleClick}
+          />
+        </div>
+      </section>
+    </>
   );
 }
